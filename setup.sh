ccabd6f7a6d2d3745c70ec3a0e136dcd82e14802
niruvibes <neil.tharol@gmail.com>
2025-06-04 16:53:14 +1000

    updated setup

--- setup.sh
+++ setup.sh
@@ -2,11 +2,18 @@
 
 # adding repos
 sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 # update list and install packages
 sudo apt update
 sudo apt install -y fastfetch
 sudo apt install -y tree tldr btop
+
+# nvm 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 20
 
 # backing up dotfiles
 if [ -f ~/.bashrc ]; then
